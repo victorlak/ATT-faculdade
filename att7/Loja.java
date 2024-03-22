4414624eef7be1fd132c02fd4d60194a5e075ac0
victor <133174073+victorlak@users.noreply.github.com>
2024-03-22 16:17:21 -0300

    Update Loja.java

--- att7/Loja.java
+++ att7/Loja.java
@@ -2,12 +2,37 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package entidades;
+package entidade;
+
+import classes.Pedido;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
  * @author 14626232639
  */
 public class Loja {
+    private List<Pedido> pedidos;
+
+    public Loja() {
+        this.pedidos = new Array<>();
+    }
+    
+    public int numeroDePedidos(){
+        int soma = 0;
+        for(Pedido p : pedidos){
+            soma++;
+        }
+        return soma;
+    }
+    public double valorTotal(){
+        double valorTotal = 0.0;
+        for(Pedido p : pedidos){
+            valorTotal = valorTotal + p.valorTotal();
+        }
+        return valorTotal;
+    }
+    
     
 }
