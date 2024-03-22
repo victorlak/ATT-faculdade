3d79e84b10c81b61d2d0bcde1942ea3e287a7f0f
victor <133174073+victorlak@users.noreply.github.com>
2024-03-22 08:43:48 -0300

    Add files via upload

---
+++ Contabancaria.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author 14626232639
+ */
+public class Contabancaria {
+    private String banco;
+    private double saldo;
+
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
+    
+    
+    
+}
