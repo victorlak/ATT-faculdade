f88f86bac7eb0159b2318eac64eec3e7ad1a0d0c
victor <133174073+victorlak@users.noreply.github.com>
2024-03-24 20:55:05 -0300

    Update Loja.java

--- att7/Loja.java
+++ att7/Loja.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package entidade;
+package entidades;
 
 import classes.Pedido;
 import java.util.ArrayList;
@@ -16,7 +16,7 @@ public class Loja {
     private List<Pedido> pedidos;
 
     public Loja() {
-        this.pedidos = new Array<>();
+        this.pedidos = new ArrayList<>();
     }
     
     public int numeroDePedidos(){
@@ -33,6 +33,34 @@ public class Loja {
         }
         return valorTotal;
     }
-    
+    public String clienteComMaisPedidos(){
+        
+        String clienteRetorno = "";
+        int controle = 0;
+        for(int i = 0; i < pedidos.size(); i++){
+            int quantidadePedidos = 0;
+            String nome = pedidos.get(i).getCliente().getNome();
+            for(int j = 0; j < pedidos.size(); j++){
+                  if(pedidos.get(i).getCliente().getNome().equals(pedidos.get(j).getCliente().getNome()))
+                  quantidadePedidos++;
+            }
+            if(quantidadePedidos > controle){
+                  controle = quantidadePedidos;
+                  clienteRetorno = nome;
+            }
+        }
+        return clienteRetorno;
+    }
+    public String maiorGasto(){
+        double maiorGasto = 0.0;
+        String nome = "";
+        for(Pedido p : pedidos){
+            if(p.valorTotal() > maiorGasto){
+                maiorGasto = p.valorTotal();
+                nome = p.getCliente().getNome();
+            }
+        }
+        return nome;
+    }
     
 }
