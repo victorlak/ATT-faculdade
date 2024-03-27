dcf0541a5345167d9721aefd273f29360cba9534
victor <133174073+victorlak@users.noreply.github.com>
2024-03-27 15:35:25 -0300

    Update Estudante.java

--- Estudante.java
+++ Estudante.java
@@ -4,10 +4,63 @@
  */
 package entidades;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author 14626232639
  */
 public class Estudante {
+    private String nome;
+    private String numeroMatricula;
+    private List<Curso> cursos;
+
+    public Estudante() {
+        this.nome ="";
+        this.numeroMatricula="";
+        cursos = new ArrayList<>();
+    }
+
+    public Estudante(String nome, String numeroMatricula, List<Curso> cursos) {
+        this.nome = nome;
+        this.numeroMatricula = numeroMatricula;
+        this.cursos = cursos;
+    }
+    public void cancelarMatricula(Curso curso){
+        cursos.remove(curso);
+    }
+    public void matricular(Curso curso){
+        cursos.add(curso);
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumeroMatricula() {
+        return numeroMatricula;
+    }
+
+    public void setNumeroMatricula(String numeroMatricula) {
+        this.numeroMatricula = numeroMatricula;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    @Override
+    public String toString() {
+        return "Estudante{" + "nome=" + nome + ", numeroMatricula=" + numeroMatricula + ", cursos=" + cursos + '}';
+    }
+    
     
 }
