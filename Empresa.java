11b214d02790fd090df8a0ecaf9f3a5ad1333e68
victor <133174073+victorlak@users.noreply.github.com>
2024-03-27 14:21:49 -0300

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -4,6 +4,7 @@
  */
 package Entidades;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -15,6 +16,44 @@ public class Empresa {
     private String nome;
     private List<Funcionario> funcionarios;
     private List<Projeto> projetos;
-    
+
+    public Empresa() {
+        funcionarios = new ArrayList<>();
+        projetos = new ArrayList<>();
+    }
+
+    public Empresa(String nome, List<Funcionario> funcionarios, List<Projeto> projetos) {
+        this.nome = nome;
+        this.funcionarios = funcionarios;
+        this.projetos = projetos;
+    }
+    public void addProjeto(Projeto projeto){
+        projetos.add(projeto);
+    }
+    public void removeProjeto(Projeto projeto){
+        projetos.remove(projeto);
+    }
+    public Funcionario maisProdutivo(){
+        int controle = 0;
+        Funcionario funcionarioMaisProdutivo = null;
+        for(Funcionario f : funcionarios){
+            if(f.numerosDeProjetos()> controle){
+                controle = f.numerosDeProjetos();
+                funcionarioMaisProdutivo = f;
+            }
+        }
+        return funcionarioMaisProdutivo;
+    }
+    public Funcionario menosProdutivo(){
+        int controle = Integer.MAX_VALUE;
+        Funcionario funcionarioMenosProdutivo = null;
+        for(Funcionario f : funcionarios){
+            if(f.numerosDeProjetos()< controle){
+                controle = f.numerosDeProjetos();
+                funcionarioMenosProdutivo = f;
+            }
+        }
+        return funcionarioMenosProdutivo;
+    }
     
 }
