260969580c12acf8d22cad66abaedc355e56ec05
victor <133174073+victorlak@users.noreply.github.com>
2024-04-05 10:51:55 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author 14626232639
+ */
+public class Animal {
+    private String nome;
+    private String especie;
+    private String raca;
+    private int idade;
+    private double peso;
+
+    public Animal(String nome, String especie, String raca, int idade, double peso) {
+        this.nome = nome;
+        this.especie = especie;
+        this.raca = raca;
+        this.idade = idade;
+        this.peso = peso;
+    }
+
+    public Animal() {
+        this.nome = "";
+        this.especie = "";
+        this.idade = 0;
+        this.peso = 0.0;
+        this.raca = "";
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void setEspecie(String especie) {
+        this.especie = especie;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    
+    @Override
+    public String toString() {
+        return "Animal{" + "nome=" + nome + ", especie=" + especie + ", raca=" + raca + ", idade=" + idade + ", peso=" + peso + '}';
+    }
+    
+    
+}
