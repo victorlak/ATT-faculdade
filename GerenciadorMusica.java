e1a51d36d03d1d7d2af627c822a43e30f750c9d7
victor <133174073+victorlak@users.noreply.github.com>
2024-04-06 17:05:39 -0300

    Add files via upload

---
+++ GerenciadorMusica.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class GerenciadorMusica {
+    private List<Musica> musicas;
+
+    public GerenciadorMusica(List<Musica> musicas) {
+        this.musicas = musicas;
+    }
+
+}
