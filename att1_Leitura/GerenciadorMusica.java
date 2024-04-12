ae423af8e908c765f4fcbbf12591a7be720d92de
victor <133174073+victorlak@users.noreply.github.com>
2024-04-12 10:11:57 -0300

    Update GerenciadorMusica.java

--- att1_Leitura/GerenciadorMusica.java
+++ att1_Leitura/GerenciadorMusica.java
@@ -6,5 +6,18 @@ public class GerenciadorMusica {
     public GerenciadorMusica(List<Musica> musicas) {
         this.musicas = musicas;
     }
+    public void addMusica(Musica musica){
+         musicas.add(musica);   
+    }
+    public void removeMusica(String nome){
+        if(buscar(nome))
+            musicas.remove(nome);
+    }
+    public void salvarNoArquivo(String caminhoDoArquivo) {
+        
+    }
 
+    public void carregarDoArquivo(String caminhoDoArquivo) {
+        
+    }
 }
